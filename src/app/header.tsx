72a93bfec428024f9b3ed2447fa397e0eb956ce4
tam11a <ibrahimsadiktamim@gmail.com>
2024-02-03 00:11:50 +0600

    header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-	return <header>Header</header>;
+	return (
+		<header className="flex flex-row items-center justify-between px-7 py-3 max-w-7xl mx-auto">
+			<div className="flex-1" />
+			<Image
+				src="/logo.svg"
+				alt="logo"
+				width={120}
+				height={100}
+				className="pb-4"
+			/>
+			<div className="flex-1 flex flex-row items-center justify-end">
+				<input
+					className="max-w-64 md:min-w-64 bg-gray-900 text-white px-5 py-2 rounded-full outline-none"
+					placeholder="Search..."
+				/>
+			</div>
+		</header>
+	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={jost.className}>
+			<body className={`bg-gray-950 text-white ${jost.className}`}>
 				<Header />
 				{children}
 				<Footer />
